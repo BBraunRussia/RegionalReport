6b8c5f5aec8d704f7be4b1e8e6c8271ea4a5ada3
Pavel Maslyaev <pavel-max@rambler.ru>
2016-02-04 17:04:29 +0300

    добавил выгрузку в Excel

---
+++ ClassLibrary/SF/DataTableToExcel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using ClassLibraryBBAuto;
+
+namespace ClassLibrary.SF
+{
+    public class CreateExcel
+    {
+        private DataTable _dt;
+
+        public CreateExcel(DataTable dt)
+        {
+            _dt = dt;
+        }
+
+        public void Show()
+        {
+            ExcelDoc excel = new ExcelDoc();
+
+            CreateHeader(excel);
+
+            CreateBody(excel);
+
+            excel.Show();
+        }
+
+        private void CreateHeader(ExcelDoc excel)
+        {
+            int i = 1;
+
+            foreach (DataColumn column in _dt.Columns)
+            {
+                excel.setValue(i, 1, column.ColumnName);
+
+                i++;
+            }
+        }
+
+        private void CreateBody(ExcelDoc excel)
+        {
+            int i = 2;
+
+            foreach (DataRow row in _dt.Rows)
+            {
+                for (int j = 0; j < row.ItemArray.Count(); j++ )
+                {
+                    excel.setValue(i, j, row.ItemArray[j].ToString());
+                }
+
+                i++;
+            }
+        }
+    }
+}
