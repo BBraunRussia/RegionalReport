e3383a7c79ffe7b944775dfd12428258a153df7d
Pavel Maslyaev <pavel-max@rambler.ru>
2016-08-18 09:32:33 +0300

    добавил новый тест

---
+++ UnitTestProjectSF/OrganizationListTest.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using System.Collections.Generic;
+using NUnit.Framework;
+using RegionR.Views.Abstract;
+using RegionReport.Domain.SF.Abstract;
+using NSubstitute;
+using RegionalR.Presentation.Concrete;
+using ClassLibrary.SF.Entities;
+using RegionReport.Domain;
+using System.Linq;
+
+namespace UnitTestProjectSF
+{
+    [TestFixture]
+    public class OrganizationListTest
+    {
+        private IOrganizationListView view;
+        private IOrganizationRepository repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            view = Substitute.For<IOrganizationListView>();
+            
+            repository = Substitute.For<IOrganizationRepository>();
+            repository.Organizations.Returns(new Organization[] {
+                new Organization(TypeOrg.ЛПУ) {ID = 1},
+                new Organization(TypeOrg.ЛПУ) {ID = 2},
+                new Organization(TypeOrg.ЛПУ) {ID = 3}
+            }.AsQueryable());
+
+            var presenter = new OrganizationListPresenter(view, repository);
+            presenter.Run();
+        }
+
+        [Test]
+        public void Count_Is_3()
+        {
+            int count = view.Organizations.Count();
+            
+            Assert.AreEqual(3, count);
+        }
+    }
+}
